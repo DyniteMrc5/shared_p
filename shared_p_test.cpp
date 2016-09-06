a0821671b06bc5a0c12c5feff0f04313d29d1b5c
Michael Cox <michael.cox@marmalademail.com>
2016-09-06 12:55:29 +0100

    Update shared_t_test.cpp - comments

--- shared_p_test.cpp
+++ shared_p_test.cpp
@@ -1,14 +1,17 @@
-//© 2016 Michael Cox
+//Â© 2016 Michael Cox
 #include "shared_p.hpp"
 #include "gtest/gtest.h"
 #include <string>
 
 #ifdef _MSC_VER
+	// If editing in Visual Studio, define these
+	// macros to allow compilation and manual inspection.
 	#define TEST(testclass, Title) void Title()
 	#define ASSERT_EQ(X,Y)
 	#define ASSERT_NE(X,Y)
 #endif
 
+// Simple object that owns an int*
 struct Data
 {
 	// takes ownership of aData
@@ -28,11 +31,14 @@ struct Data
 	int* iData;
 };
 
+// Tests compilation
 TEST(Basics, CanCreateShPofIntPtr)
 {
 	shared_p<int> s = shared_p<int>::make_shared(new int(5));
 }
 
+// Tests that d is set to null after shared_p is created
+// Tests that int* is deleted (value is set to something other than 5) (accessing deleted* is technically undefined behaviour)
 TEST(Basics, CanCreateShPOfData)
 {
 	int* five = new int(5);
@@ -45,6 +51,7 @@ TEST(Basics, CanCreateShPOfData)
 	ASSERT_NE(*five, 5);
 }
 
+// Tests multiple shared_p
 TEST(Basics, CanCreate2ShP)
 {
 	int* five = new int(5);
@@ -60,6 +67,7 @@ TEST(Basics, CanCreate2ShP)
 	ASSERT_NE(*five, 5);
 }
 
+// Test multiple shared_p with different object lifetimes
 TEST(Basics, CanCreate2ShP_DiffSharedLifetimes)
 {
 	int* five = new int(5);
@@ -70,12 +78,16 @@ TEST(Basics, CanCreate2ShP_DiffSharedLifetimes)
 
 		{
 			shared_p<Data> t = s;
-		}
+		} // t destroyed here
+		
+		// five should still be 5
 		ASSERT_EQ(*five, 5);
-	}
+	} // s deleted here
+	
 	ASSERT_NE(*five, 5);
 }
 
+// Test operator T& works
 TEST(Basics, TestOperatorT)
 {
 	int* five = new int(5);
@@ -93,6 +105,7 @@ TEST(Basics, TestOperatorT)
 	ASSERT_NE(*five, 5);
 }
 
+// Test T& get() works
 TEST(Basics, TestOperatorGet)
 {
 	int* five = new int(5);
@@ -115,6 +128,7 @@ void deleteFn(Data* aData)
 	*aData->iData = 7;
 }
 
+//Test with custom deleter
 TEST(Basics, TestDeleter)
 {
 	int* five = new int(5);
@@ -136,6 +150,7 @@ void DataFn(Data& aData)
 	*aData.iData = abcdef;
 }
 
+//Test conversion to Data& automatically
 TEST(Basics, TestCanCallFunctionWithoutSharedPSigniture)
 {
 	int* five = new int(5);
